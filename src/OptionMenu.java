b143bf2910e89a46419188293016089b08c185bc
m1ras01 <221930@astanait.edu.kz>
2023-10-13 11:32:10 +0600

    methods GetSaving and getChecking:)

--- src/OptionMenu.java
+++ src/OptionMenu.java
@@ -34,7 +34,82 @@ public class OptionMenu extends Account{
         }while (x==1);
     }
     public void getAccountType(){
-        System.out.println("welcome");
+        System.out.println("Select the Account you want access:");
+        System.out.println("Type 1 - Checking Account");
+        System.out.println("Type 2 - Saving Account");
+        System.out.println("Type 3 - Exit");
+        System.out.println("Choose:");
+        int n = menuInput.nextInt();
+
+        switch (n){
+            case 1:
+                getChecking();
+                break;
+            case 2:
+                getSaving();
+                break;
+            case 3:
+                System.out.println("Thank you for using ATM");
+            default:
+                System.out.println("\nInvalid choice\n");
+                getAccountType();
+        }
+    }
+    public void getChecking(){
+        System.out.println("Checking Account:");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Funds");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+        System.out.println("Choice:");
+        int n = menuInput.nextInt();
+        switch (n){
+            case 1:
+                System.out.println("Checking Account Balance:"+moneyFormat.format(getCheckingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getCheckingWidthDraw();
+                getAccountType();
+                break;
+            case 3:
+                getCheckingDeposit();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for using ATM");
+            default:
+                System.out.println("\nInvalid choice\n");
+                getChecking();
+        }
+    }
+    public void getSaving(){
+        System.out.println("Saving Account:");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Funds");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+        System.out.println("Choice:");
+        int n = menuInput.nextInt();
+        switch (n){
+            case 1:
+                System.out.println("Saving Account Balance:"+moneyFormat.format(getSavingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getSavingWidthDraw();
+                getAccountType();
+                break;
+            case 3:
+                getSavingDeposit();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for using ATM");
+            default:
+                System.out.println("\nInvalid choice\n");
+                getSaving();
+        }
     }
 }
 
